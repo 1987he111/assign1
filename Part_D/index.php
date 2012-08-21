5c8f3e05a7c34ecff2f96d8c1c032e562cbd6677
s3266174 <1987he111@sina.com>
2012-08-21 17:14:49 +1000

    this is for Part_D

---
+++ Part_D/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h2><a href="search.php">search</a></h2>
+        <h2><a href="answer.php">answer</a></h2>
+    </body>
+</html>
